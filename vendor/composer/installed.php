170ea5072e8d62c88b8c46d50981bcef0baf52b9
miyaokana <k.miyao@tbc-inc.co.jp>
2026-06-29 14:44:51 +0900

    miyao ブランチー

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tbc-admin/team1',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '012efcb338c02476c1180625d707903794f41575',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tbc-admin/team1' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '012efcb338c02476c1180625d707903794f41575',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
