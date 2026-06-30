995264975d4a33bbbd9618b70f0ce4eae5687bbb
Masato Tamura <715-student012@la-study.com>
2026-06-30 16:58:32 +0900

    とうやマージしたよ

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\ShiftController;
 
